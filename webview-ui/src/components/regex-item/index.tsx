ed154af417a2dfe940b027f3bd7088822e6191ba
Kenny Ye <kennyye1998@gmail.com>
2024-06-05 22:31:40 +1000

    Expanding sections

--- webview-ui/src/components/regex-item/index.tsx
+++ webview-ui/src/components/regex-item/index.tsx
@@ -1,7 +1,16 @@
-import React from "react";
-import { ButtonContainer, Chevron, Container, RegexDisplay, Title, TitleName } from "./styled";
+import React, { useCallback, useMemo, useRef, useState } from "react";
+import {
+  ButtonContainer,
+  ChevronContainer,
+  Container,
+  Content,
+  ContentInner,
+  RegexDisplay,
+  Title,
+  TitleName,
+} from "./styled";
 import { VSCodeTextField, VSCodeDivider, VSCodeButton } from "@vscode/webview-ui-toolkit/react";
-import {} from "material-icons";
+import { FaChevronRight } from "react-icons/fa";
 
 type RegexItemProps = {
   name: string;
@@ -9,19 +18,36 @@ type RegexItemProps = {
 };
 
 export const RegexItem = ({ name, regex }: RegexItemProps) => {
+  const contentRef = useRef<HTMLDivElement | null>(null);
+  const [isExpanded, setIsExpanded] = useState<boolean>(false);
+
+  const onTitleClick = useCallback(() => {
+    setIsExpanded((currentIsExpanded) => !currentIsExpanded);
+  }, []);
+
+  const contentHeight = contentRef.current?.clientHeight || 0;
+
+  console.log({ contentHeight });
+
   return (
     <Container>
-      <Title>
+      <Title onClick={onTitleClick}>
         <TitleName>{name}</TitleName>
-        <Chevron>{">"}</Chevron>
+        <ChevronContainer isExpanded={isExpanded}>
+          <FaChevronRight />
+        </ChevronContainer>
       </Title>
-      <RegexDisplay>{regex}</RegexDisplay>
-      <VSCodeTextField placeholder={"filename"} />
-      <VSCodeTextField placeholder={"pathname"} />
-      <ButtonContainer>
-        <VSCodeButton appearance={"secondary"}>File search</VSCodeButton>
-        <VSCodeButton appearance={"primary"}>Global search</VSCodeButton>
-      </ButtonContainer>
+      <Content isExpanded={isExpanded} maxHeight={contentHeight}>
+        <ContentInner ref={contentRef}>
+          <RegexDisplay>{regex}</RegexDisplay>
+          <VSCodeTextField placeholder={"filename"} />
+          <VSCodeTextField placeholder={"pathname"} />
+          <ButtonContainer>
+            <VSCodeButton appearance={"secondary"}>File search</VSCodeButton>
+            <VSCodeButton appearance={"primary"}>Global search</VSCodeButton>
+          </ButtonContainer>
+        </ContentInner>
+      </Content>
       <VSCodeDivider />
     </Container>
   );
--- webview-ui/src/components/regex-item/styled.tsx
+++ webview-ui/src/components/regex-item/styled.tsx
@@ -4,18 +4,35 @@ export const Container = styled.div({
   display: "flex",
   flexDirection: "column",
   justifyContent: "space-between",
-  gap: "5px",
 });
 
 export const Title = styled.div({
   fontSize: "16px",
   display: "flex",
   justifyContent: "space-between",
+  alignItems: "center",
+  cursor: "pointer",
+  userSelect: "none",
 });
 
 export const TitleName = styled.div({});
 
-export const Chevron = styled.div({});
+export const ChevronContainer = styled.div<{ isExpanded: boolean }>((props) => ({
+  transform: props.isExpanded ? "rotate(90deg)" : "rotate(0deg)",
+  transition: "transform 0.2s ease-out",
+}));
+
+export const Content = styled.div<{ isExpanded: boolean; maxHeight: number }>((props) => ({
+  maxHeight: props.isExpanded ? `${props.maxHeight}px` : "0",
+  transition: "all 0.2s ease-out",
+  overflow: "hidden",
+}));
+
+export const ContentInner = styled.div({
+  display: "flex",
+  flexDirection: "column",
+  gap: "5px",
+});
 
 export const RegexDisplay = styled.div({
   fontSize: "20px",
